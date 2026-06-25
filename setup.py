341783170b866a28c840c387aa9f9eeca15a5670
Shivek Gandhi <shivekgandhi1010@gmail.com>
2026-06-26 00:49:58 +0530

    jupyter-notebook-done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='shivek-gandhi',
+    author_email='sshivekgandhi@gmail.com',
+    packages = find_packages(),
+    install_requires=[]
+)
